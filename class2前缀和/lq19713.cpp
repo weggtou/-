5dc389279c4405de121e4fa8dea70ca5b0457c22
weggt <3044129132@qq.com>
2025-10-26 14:31:09 +0800

    1

--- class2前缀和/lq19713.cpp
+++ class2前缀和/lq19713.cpp
@@ -10,17 +10,30 @@ struct __X {
 int main(){
   int n;
   cin >> n;
-  vector <long long> all(n),s(n);
-  long long sum = 0;
+  vector <long long> all(n),s(n+1);
   for(auto &i:all){
     cin >> i;
-    sum+=i;
-    i*=2;
   }
-  s[0] = sum;
-  for(int i = 1;i<n;i++){
-    s[i] = s[i-1]-all[i-1];
+  partial_sum(all.begin(),all.end(),s.begin()+1);
+  set <long long> jian;
+  long long ans=LLONG_MAX;
+  for(int r = n-1;r>0;r--){
+    for(int x = r+1;x<=n;x++){
+      jian.insert(s[x]-s[r]);
+    }
+    for(int l = 1;l<=r;l++){
+      long long x = s[r]-s[l-1];
+      auto iter = jian.lower_bound(x);
+      if(iter!=jian.end()){
+        ans = min(ans,*iter - x);
+      }
+      if(iter!=jian.begin()){
+        iter--;
+        ans = min(ans,x-*iter);
+      }
+    }
   }
-  partial_sum(all.begin(),all.end(),all.begin());
+  cout << ans << endl;
+
   
 }
