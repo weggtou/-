e0b1fa67841aaa562937b3aed292831ca5a0dbd9
weggt <3044129132@qq.com>
2025-11-10 20:32:09 +0800

    1

--- class3差分/2132.stamping-the-grid.cpp
+++ class3差分/2132.stamping-the-grid.cpp
@@ -14,58 +14,40 @@ using namespace std;
 class Solution {
 public:
     bool possibleToStamp(vector<vector<int>>& g, int H, int W) {
-        int m = g.size(),n=g[0].size(),miw=INT32_MAX,mih=INT32_MAX;
-        for(int i = 0;i<m;i++){
-            vector <int> t;
-            for(int j = 0;j<n;j++){
-                if(g[i][j]==1){
-                    g[i][j]=0;
-                    if(j!=0){
-                        t.push_back(g[i][j-1]);
-                    }
-                }else{
-                    if(j!=0){
-                        g[i][j]=g[i][j-1]+g[i][j];
-                    }
-                }
-            }
-            t.push_back(g[i][n-1]);
-            ranges::sort(t);
-            for(auto &k:t){
-                if(k){
-                    miw=k<miw?k:miw;
-                    break;
-                }
-            }
-        }
+        int n = g.size();int m = g[0].size();
+        auto all = vector(n+2,vector<int>(m+2));
+        auto all2 = vector(n+2,vector<int>(m+2));
         for(int i=0;i<n;i++){
-            vector <int> t;
+            partial_sum(g[i].begin(),g[i].end(),all[i+1].begin()+1);
+            for(int j=1;j<=m;j++){
+                all[i+1][j]=all[i][j]+all[i+1][j];
+            }
+        }
+        for(int i = 0;i+H<=n;i++){
+            for(int j=0;j+W<=m;j++){
+                int sum = all[i][j]+all[i+H][j+W]-all[i][j+W]-all[i+H][j];
+                if(sum == 0){
+                    all2[i+1][j+1]++;
+                    all2[i+H+1][j+W+1]++;
+                    all2[i+H+1][j+1]--;
+                    all2[i+1][j+W+1]--;
+                }
+            }
+        }
+        for(int i = 0;i<all2.size();i++){
+            partial_sum(all2[i].begin(),all2[i].end(),all2[i].begin());
+            for(int j = 0;j<all2[0].size();j++){
+                if(i)all2[i][j] = all2[i][j]+all2[i-1][j];
+            }
+        }
+        for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
-                if(g[j][i]==1){
-                    g[j][i]=0;
-                    if(j!=0){
-                        t.push_back(g[j-1][i]);
-                    }
-                }else{
-                    if(j!=0){
-                        g[j][i]=g[j-1][i]+g[j][i];
-                    }
-                }
-            }
-            t.push_back(g[m-1][i]);
-            ranges::sort(t);
-            for(auto &k:t){
-                if(k){
-                    mih=k<mih?k:mih;
-                    break;
+                if(all2[i+1][j+1]==0&&g[i][j]==0){
+                    return false;
                 }
             }
         }
-        if(H<=mih&&W<=miw){
-            return 1;
-        }else{
-            return 0;
-        }
+        return true;
     }
 };
 // @lc code=end
