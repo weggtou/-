d51e88a92d06fa420b3d08e3b5ebd49967e0b60c
weggt <3044129132@qq.com>
2025-11-25 21:19:49 +0800

    1

--- class4单调性枚举/lq179.cpp
+++ class4单调性枚举/lq179.cpp
@@ -1,5 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
+  using ll = long long;
+  int n,d,k;
+  cin >> n >> d >> k;
+  vector <pair<ll,ll>> all(n);
+  for(auto &[t,i]:all){
+    cin >> t >> i;
+  }
+  sort(all.begin(),all.end());
+  set <int> ans;
+  vector <ll> id(1e5+1);
+  for(int l=0,r=0;l<n;){
+    while(r<n&&all[r].first-all[l].first<d){
+      id[all[r].second]++;
+      if(id[all[r].second]>=k) ans.insert(all[r].second);
+      r++;
+    }
+    id[all[l++].second]--;
+  }
+  for(auto i:ans){
+    cout << i << endl;
+  }
 }
