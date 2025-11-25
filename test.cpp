6fd9e7fe7cb2c8cb1cbddfc59896649deb778550
weggt <3044129132@qq.com>
2025-11-25 19:24:49 +0800

    1

--- test.cpp
+++ test.cpp
@@ -3,9 +3,14 @@ using namespace std;
 int main(){
   vector <int> a(10);
   a={1,2,3,4,5};
-  for(int i =0;i<5;){
-    int x = a[i++];
+  int i;
+  for(i =0;i<5;i++){
+    int x = a[i];
     cout << x;
+    if(i==3){
+      break;
+    }
   }
+  cout << i;
   
 }
