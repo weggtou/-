178e75f00eefda69060303bf9b82e1ea7e1e1d20
wegpad <3044129132@qq.com>
2026-02-03 17:05:50 +0800

    1

---
+++ class4单调性枚举/p10444.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int t,id;
+  cin >> t>>id;
+  while(t--){
+    int n;
+    cin >> n;
+    vector <int> a(n);
+    for(auto &i:a){
+      cin >> i;
+    }
+    ranges::sort(a);
+    for(int l = 0,r = 0;){
+
+    }
+}
